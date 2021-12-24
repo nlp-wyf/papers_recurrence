2d20cf7bd5713558e2b9d69f431be9502afd8380
wyf <1546563183@qq.com>
2021-12-24 15:20:51 +0800

    add the Kamming's MAE

---
+++ My_MAE/MAE.py
@@ -0,0 +1,121 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from einops import repeat
+
+from My_MAE.ViT import Transformer, ViT
+
+
+class MAE(nn.Module):
+    def __init__(
+            self,
+            *,
+            encoder,
+            decoder_dim,
+            masking_ratio=0.75,
+            decoder_depth=1,
+            decoder_heads=8,
+            decoder_dim_head=64
+    ):
+        super().__init__()
+        assert 0 < masking_ratio < 1, 'masking ratio must be kept between 0 and 1'
+        self.masking_ratio = masking_ratio
+
+        # extract some hyper-parameters and functions from encoder (vision transformer to be trained)
+        self.encoder = encoder
+        num_patches, encoder_dim = encoder.pos_embedding.shape[-2:]
+        self.to_patch, self.patch_to_emb = encoder.to_patch_embedding[:2]
+        pixel_values_per_patch = self.patch_to_emb.weight.shape[-1]
+
+        # decoder parameters
+        self.enc_to_dec = nn.Linear(encoder_dim, decoder_dim) if encoder_dim != decoder_dim else nn.Identity()
+        self.mask_token = nn.Parameter(torch.randn(decoder_dim))
+        self.decoder = Transformer(dim=decoder_dim, depth=decoder_depth, heads=decoder_heads, dim_head=decoder_dim_head,
+                                   mlp_dim=decoder_dim * 4)
+        self.decoder_pos_emb = nn.Embedding(num_patches, decoder_dim)
+        self.to_pixels = nn.Linear(decoder_dim, pixel_values_per_patch)
+
+    def forward(self, img):
+        # img.shape [batch_size, channels, height, width]==[1,3, 224,224]
+        # get patches, patches.shape [batch_size, num_patches, patch_dim]
+        patches = self.to_patch(img)
+        batch, num_patches, *_ = patches.shape
+
+        # patch to encoder tokens and add positions
+        # tokens.shape [batch_size, num_patches, dim]
+        tokens = self.patch_to_emb(patches)
+        tokens = tokens + self.encoder.pos_embedding[:, 1:(num_patches + 1)]
+
+        # calculate of patches needed to be masked, and get random indices,
+        # dividing it up for mask vs unmasked
+        num_masked = int(self.masking_ratio * num_patches)
+        # rand_indices.shape [batch, num_patches]
+        rand_indices = torch.rand(batch, num_patches).argsort(dim=-1)
+        masked_indices, unmasked_indices = rand_indices[:, :num_masked], rand_indices[:, num_masked:]
+
+        # get the unmasked tokens to be encoded
+        # batch_range.shape [batch, 1]
+        batch_range = torch.arange(batch)[:, None]
+        # tokens.shape [batch, unmasked_patches, dim]
+        tokens = tokens[batch_range, unmasked_indices]
+
+        # get the patches to be masked for the final reconstruction loss
+        # masked_patches.shape [batch, masked_patches, dim]
+        masked_patches = patches[batch_range, masked_indices]
+
+        # attend with vision transformer
+        encoded_tokens = self.encoder.transformer(tokens)
+
+        # project encoder to decoder dimensions, if they are not equal
+        # the paper says you can get away with a smaller dimension for decoder
+        # decoder_tokens.shape [batch, unmasked_patches, dim]
+        decoder_tokens = self.enc_to_dec(encoded_tokens)
+
+        # repeat mask tokens for number of masked, and add
+        # the positions using the masked indices derived above
+        mask_tokens = repeat(self.mask_token, 'd -> b n d', b=batch, n=num_masked)
+        mask_tokens = mask_tokens + self.decoder_pos_emb(masked_indices)
+
+        # concat the masked tokens to the decoder tokens and attend with decoder
+        decoder_tokens = torch.cat((mask_tokens, decoder_tokens), dim=1)
+        # decoder_tokens.shape [batch, num_patches, dim]
+        decoded_tokens = self.decoder(decoder_tokens)
+
+        # splice out the mask tokens and project to pixel values
+        mask_tokens = decoded_tokens[:, :num_masked]
+        pred_pixel_values = self.to_pixels(mask_tokens)
+
+        # calculate reconstruction loss
+        # masked_patches.shape [batch, masked_patches, dim]
+        recon_loss = F.mse_loss(pred_pixel_values, masked_patches)
+        return recon_loss, pred_pixel_values
+
+
+vit = ViT(
+    image_size=224,
+    patch_size=16,
+    num_classes=1000,
+    dim=1024,
+    depth=6,
+    heads=16,
+    mlp_dim=2048,
+    dropout=0.1,
+    emb_dropout=0.1
+)
+
+mae = MAE(
+    encoder=vit,
+    decoder_dim=512,
+    masking_ratio=0.75,
+    decoder_depth=1,
+    decoder_heads=8,
+    decoder_dim_head=64
+)
+
+img = torch.randn(1, 3, 224, 224)
+
+
+loss, pred = mae(img)
+print(loss.item())
+# pred.shape [batch, masked_pixel_values, dim]
+print(pred.shape)
